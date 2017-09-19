40d9ff513196aaed16da96627144a9818629e45c
mudhafar <mudhaffar2@gmail.com>
2017-09-19 14:05:13 +0200

    finish

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function gibberish(input) {
   // code here
+  var strarray = input.split('');
+  var returned = strarray.map(function(letter) {
+    if (letter === "o") {
+      return "0";
+    } else if (letter === "i") {
+      return "1";
+    } else if (letter === "e") {
+      return "3";
+    } else if (letter === "a") {
+      return "4";
+    } else if (letter === "s") {
+      return "5";
+    } else {
+      return letter;
+    }
+  });
+  return returned.join('');
+
 }
 
-// don't touch below this line ;)
+// don't touch below this line :wink:
 // --------------------------------------------------------------
 
 var text = 'Un chasseur sachant chasser sans son chien est un bon chasseur';
 var converted = gibberish(text);
-var verif = 'Un ch4553ur 54ch4nt ch4553r 54n5 5on ch13n 35t un b0n ch4553ur';
+var verif = 'Un ch4553ur 54ch4nt ch4553r 54n5 50n ch13n 35t un b0n ch4553ur';
 console.info('Your text:    ' + converted,
-           '\nVerification: ' + verif);
+          '\nVerification: ' + verif);
 
 if (converted !== verif) {
-  throw new Error('PWND!')
+ throw new Error('PWND!')
 }
-console.info('👏 w311 d0n3! 👯');
+console.info(':clap: w311 d0n3! :dancers:');
+
+console.info(gibberish("Hello World!!!!"));
